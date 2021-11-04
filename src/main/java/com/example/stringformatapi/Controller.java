2ee4bef2d35fdc5f07f1ad915635ecc3a9a3e777
kamilb22 <222574@edu.p.lodz.pl>
2021-11-04 14:11:54 +0100

    Added controller class with empty methods

--- src/main/java/com/example/stringformatapi/Controller.java
+++ src/main/java/com/example/stringformatapi/Controller.java
@@ -1,15 +1,21 @@
 package com.example.stringformatapi;
 
+import com.fasterxml.jackson.databind.JsonSerializable;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
     @RequestMapping("/lowerCase/{string}")
-    public int lowerCase(@PathVariable String string, @RequestParam(required = false) String mode){
-        return 0;
+    public String lowerCase(@PathVariable String string, @RequestParam(required = false) String mode){
+        return "";
     }
 
     @RequestMapping("/upperCase/{string}")
