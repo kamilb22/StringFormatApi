33365c7351e796d75bb784d81cb8b1ff2dd48c46
kamilb22 <222574@edu.p.lodz.pl>
2021-11-04 12:44:01 +0100

    Initial commit

---
+++ src/main/java/com/example/stringformatapi/StringFormatApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stringformatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringFormatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringFormatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stringformatapi/StringFormatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stringformatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringFormatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
